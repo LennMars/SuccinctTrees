13a27f884b5f9cc12f55e5fea3b7c7c7f95e0f47
LennMars <LennMars@gmail.com>
2012-07-09 05:45:45 +0900

    Add speed tester and switching between tests

--- suc_tree_test.cc
+++ suc_tree_test.cc
@@ -1,6 +1,7 @@
 #include "suc_tree.h"
 #include "climits"
 #include "cstdlib"
+#include "sys/time.h"
 
 using std::vector;
 
@@ -71,26 +72,54 @@ void test_correctness(int size, float deepness, int index, int diff) {
     }
   } else {
     printf("i, d : %d, %d\n", index, diff);
-    uint fast = tr.fwd_search(index, diff);
     uint naive = tr.fwd_search_naive(index, diff);
-    printf("fast, naive : %d, %d\n", fast, naive);
+    printf("naive : %d, ", naive);
+    uint fast = tr.fwd_search(index, diff);
+    printf("fast : %d\n", fast);
   }
 }
 
+void test_speed_basic(int size, float deepness) {
+  cs::bitVector t;
+  make_random_tree(size, deepness, t);
+  suc_tree tr(t);
+  timeval start, end;
+  gettimeofday(&start, NULL);
+  for (uint i = 0; i < size * 2; ++i){
+    uint fast = tr.fwd_search(i, 0);
+  }
+  gettimeofday(&end, NULL);
+  float passed = (float)(end.tv_sec - start.tv_sec) * 1000000 + (float)(end.tv_usec - start.tv_usec);
+  printf("took %f usec per fwd_search\n", passed / size / 2);
+}
+
 int main(int argc, char* argv[]) {
-//   test1();
+  int test = 0;
   int size = 100;
   float deepness = 0.5;
   int index = -1;
   int diff = 0;
   if (argc > 2) {
-    size = atoi(argv[1]);
-    deepness = atof(argv[2]);
+    test = atoi(argv[1]);
   }
-  if (argc > 4) {
-    index = atoi(argv[3]);
-    diff = atoi(argv[4]);
+  if (argc > 3) {
+    size = atoi(argv[2]);
+    deepness = atof(argv[3]);
   }
-  test_correctness(size, deepness, index, diff);
+  if (argc > 5) {
+    index = atoi(argv[4]);
+    diff = atoi(argv[5]);
+  }
+  switch(test) {
+  case 0 :
+    test_correctness(size, deepness, index, diff);
+    break;
+  case 1 :
+    test_speed_basic(size, deepness);
+    break;
+  case 2 :
+    test1();
+    break;
+  }
   return 0;
 }
